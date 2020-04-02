97b66b36b2791e1d4909993a881c4f16719c4898
KimJinsu66 <jinsu6688@gmail.com>
2020-04-02 23:22:22 +0900

    Initial sharing of project

---
+++ test1.java
@@ -0,0 +1,18 @@
+
+/**
+ * 여기에 test1 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class test1
+{
+    public static void main(String[] args){
+        int a = 1;
+        int b = 1;
+        a = b++;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
