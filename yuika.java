246ad086ab04334f2b9e1deca4187b5f3622d2e2
KimJinsu66 <jinsu6688@gmail.com>
2020-04-02 23:31:12 +0900

    add yuika class

---
+++ yuika.java
@@ -0,0 +1,14 @@
+
+/**
+ * 여기에 yuika 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class yuika
+{
+    public static void main(String[] args){
+        System.out.println("fighting");
+    
+    
+    }}
